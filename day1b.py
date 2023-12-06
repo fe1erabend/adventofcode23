c1d77877d0142f4ae4e0b7e1a42f0c117c238015
Felix <felixwalther22@gmail.com>
2023-12-06 14:53:02 +0100

    optimized with word2number

--- day1b.py
+++ day1b.py
@@ -1,33 +1,13 @@
 import re
+from word2number import w2n
 
 sum = 0
-
-def parser(input):
-    if input == 'one':
-        return "1"
-    elif input == 'two':
-        return "2"
-    elif input == 'three':
-        return "3"
-    elif input == 'four':
-        return "4"
-    elif input == 'five':
-        return "5"
-    elif input == 'six':
-        return "6"
-    elif input == 'seven':
-        return "7"
-    elif input == 'eight':
-        return "8"
-    elif input == 'nine':
-        return "9"
-    else:
-        return input
     
 with open("input1a.txt") as input:
     for line in input:
         findings = {}        
-        for expression in ["one","1","two","2","three","3","four","4","five","5","six","6","seven","7","eight","8","nine","9"]:
+        for expression in ["one","1","two","2","three","3","four","4","five","5",
+                           "six","6","seven","7","eight","8","nine","9"]:
             matches = [match.start() for match in re.finditer('(?={0})'.format(re.escape(expression)), line)]
             findings[expression] = matches
         filtered_findings = {key: value for key, value in findings.items() if value}            
@@ -35,7 +15,7 @@ with open("input1a.txt") as input:
         first_value = str(min(filtered_findings,key=lambda k: min(filtered_findings[k])))
         last_value = str(max(filtered_findings, key=lambda k: max(filtered_findings[k])))
 
-        value = int(str(parser(first_value)) + str(parser(last_value)))       
+        value = int(str(w2n.word_to_num(first_value)) + str(w2n.word_to_num(last_value)))       
         sum += value
 print(sum)    
             
