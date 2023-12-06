772fefa40348d2f9e9a84dbafc74e9c03d4be78d
Felix <felixwalther22@gmail.com>
2023-12-06 14:38:53 +0100

    solved day 1b

--- day1b.py
+++ day1b.py
@@ -1,13 +1,47 @@
+import re
+
 sum = 0
+
+def parser(input):
+    if input == 'one':
+        return "1"
+    elif input == 'two':
+        return "2"
+    elif input == 'three':
+        return "3"
+    elif input == 'four':
+        return "4"
+    elif input == 'five':
+        return "5"
+    elif input == 'six':
+        return "6"
+    elif input == 'seven':
+        return "7"
+    elif input == 'eight':
+        return "8"
+    elif input == 'nine':
+        return "9"
+    else:
+        return input
+    
 with open("input1a.txt") as input:
     for line in input:
-        digitstring = ""
-        # for char in line:            
-        #     if char.isdigit():
-        #         digitstring += char
+        findings = {}
+        matches = []
+        filtered_findings = {}
+        for expression in ["one","1","two","2","three","3","four","4","five","5","six","6","seven","7","eight","8","nine","9"]:
+            matches = [match.start() for match in re.finditer('(?={0})'.format(re.escape(expression)), line)]
+            findings[expression] = matches
+        filtered_findings = {key: value for key, value in findings.items() if value}
+        #print(filtered_findings)        
 
-        x = line.find("two")
+        first_value = str(min(filtered_findings,key=lambda k: min(filtered_findings[k])))
+        last_value = str(max(filtered_findings, key=lambda k: max(filtered_findings[k])))
 
-        #digits = digitstring[0] + digitstring[-1]
-        #sum += int(digits)    
-print(x)             
+        digits = str(parser(first_value)) + str(parser(last_value))
+
+        value = int(digits)    
+        #print(value)
+        sum += value
+print(sum)    
+            
